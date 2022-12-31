3ebdd69bd646de4f9ed39ff7ea9240cb24877de9
skyrred <sky.red2212@gmail.com>
2022-12-31 17:33:16 +0100

    updated homepage

--- frontend/rentalApp/src/main.jsx
+++ frontend/rentalApp/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/general/index.css'
+import './styles/modular/header.css'
+import './styles/modular/homepage.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ frontend/rentalApp/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+    },
+    colors: {
+      transparent: 'transparent',
+      mainBlue : '#3563e9',
+      white : '#ffffff',
+    },
+  },
+  plugins: [],
+}
